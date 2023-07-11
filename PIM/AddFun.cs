6bf3794d7f1a50813229bd5dd2ef29158c1d0322
VitoriaCardozo <126196994+Bitroia@users.noreply.github.com>
2023-07-10 22:30:40 -0300

    Correcão de funções

---
+++ PIM/AddFun.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PIM_CASI
+{
+    public partial class AddFun : Form
+    {
+        // C R I A R   C O N E X A O
+
+        private MySqlConnection Conexao;
+        private string data_source = "datasource=127.0.0.1;username=root;password=010254;database=PIM";
+
+
+        public AddFun()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddFun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);
+                
+                Conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = Conexao;
+
+                // A D D    F U N C I O N A R I O
+
+                cmd.CommandText = "INSERT INTO FUNCIONARIO( " +
+                    "NOME_FUNCIONARIO, " +
+                    "CPF_FUNCIONARIO, " +
+                    "FONE_FUNCIONARIO, " +
+                    "CARGO_FUNCIONARIO, " +
+                    "VALOR_HORA, " +
+                        "RUA," +
+                        "COMPLEMENTO, " +
+                        "NUMERO, " +
+                        "BAIRRO, " +
+                        "UF, " +
+                        "CIDADE) " +
+                    "VALUES( @NOME_FUNCIONARIO," +
+                    "@CPF_FUNCIONARIO," +
+                    "@FONE_FUNCIONARIO," +
+                    "@CARGO_FUNCIONARIO," +
+                    "@VALOR_HORA_FUNCIONARIO," +
+                    "@RUA," +
+                    "@COMPLEMENTO," +
+                    "@NUMERO," +
+                    "@BAIRRO," +
+                    "@UF," +
+                    "@CIDADE)";
+
+                cmd.Parameters.AddWithValue("@NOME_FUNCIONARIO",txtNome.Text);
+                cmd.Parameters.AddWithValue("@CPF_FUNCIONARIO",txtCPF.Text);
+                cmd.Parameters.AddWithValue("@FONE_FUNCIONARIO",txtFone.Text);
+                cmd.Parameters.AddWithValue("@CARGO_FUNCIONARIO",txtCargo.Text);
+                cmd.Parameters.AddWithValue("@VALOR_HORA_FUNCIONARIO",txtValorHora.Text);
+                cmd.Parameters.AddWithValue("@RUA",txtRua.Text);
+                cmd.Parameters.AddWithValue("@COMPLEMENTO",txtComplemento.Text);
+                cmd.Parameters.AddWithValue("@NUMERO",txtNumero.Text);
+                cmd.Parameters.AddWithValue("@BAIRRO",txtBairro.Text);
+                cmd.Parameters.AddWithValue("@UF",txtUF.Text);
+                cmd.Parameters.AddWithValue("@CIDADE", txtCidade.Text);
+                
+                cmd.Prepare();
+                
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionario foi criado com sucesso!", "Confirmação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtNome.Text = string.Empty;
+                txtCPF.Text = string.Empty;
+                txtFone.Text = string.Empty;
+                txtCargo.Text = string.Empty;
+                txtValorHora.Text = string.Empty;
+                txtRua.Text = string.Empty;
+                txtComplemento.Text = string.Empty;
+                txtNumero.Text = string.Empty;
+                txtBairro.Text = string.Empty;
+                txtUF.Text = string.Empty;
+                txtCidade.Text = string.Empty;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally { 
+                Conexao.Close(); 
+            }
+        }
+    }
+}
---
+++ PIM/Ponto.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace PIM_CASI
+{
+    partial class Ponto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(962, 80);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ponto de Funcionario";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Ponto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.ClientSize = new System.Drawing.Size(962, 753);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Ponto";
+            this.Text = "Ponto";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
